3a1c6d6d3664c2fadb7746b76406a567daeab7e8
trodevel <trodevel@gmail.com>
2020-11-28 04:17:46 +0100

    output refinement

--- helpers.py
+++ helpers.py
@@ -25,18 +25,18 @@ def find_element_by_tag_name_and_attribute_name( driver, tag_name, attribute_nam
 
     all_elems = driver.find_elements_by_tag_name( tag_name )
 
-    print( "DEBUG: all '{}' {}:".format( tag_name, len( all_elems ) ) )
+    print( "DEBUG: find_element_by_tag_name_and_attribute_name: all '{}' {}:".format( tag_name, len( all_elems ) ) )
 
     for i in all_elems:
         i_val = i.get_attribute( attribute_name )
-        print ( "DEBUG: {} '{}' '{}'".format( i.text, attribute_name, i_val ) )
+        print ( "DEBUG: find_element_by_tag_name_and_attribute_name: {} '{}' '{}'".format( i.text, attribute_name, i_val ) )
         if is_whole_name:
             if i_val == attribute_val:
-                print( "DEBUG: FOUND - {}".format( i_val ) )
+                print( "DEBUG: find_element_by_tag_name_and_attribute_name: FOUND - {}".format( i_val ) )
                 return i
         else:
             if i_val.startswith( attribute_val ):
-                print( "DEBUG: FOUND - {} ".format( i_val ) )
+                print( "DEBUG: find_element_by_tag_name_and_attribute_name: FOUND - {} ".format( i_val ) )
                 return i
 
     return None;
